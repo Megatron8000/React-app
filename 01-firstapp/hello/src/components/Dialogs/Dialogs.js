5a3dd8153a2879e27142f11fd3ce164bca758eda
Alexander <barykin88@hotmail.com>
2020-06-01 22:17:14 +0400

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'React-app/master'.
    
     Changes to be committed:
    	modified:   src/components/Dialogs/Dialogs.js
    	modified:   src/components/Dialogs/Dialogs.module.css
    
    Begin styling messages

--- 01-firstapp/hello/src/components/Dialogs/Dialogs.js
+++ 01-firstapp/hello/src/components/Dialogs/Dialogs.js
@@ -1,10 +1,31 @@
 import React from 'react';
-// import classes from './dialogs.module.css';
+import classes from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog + ' ' + classes.active}>
+                    Jack
+                </div>
+                <div className={classes.dialog}>
+                    James
+                </div>
+                <div className={classes.dialog}>
+                    Jill
+                </div>
+                <div className={classes.dialog}>
+                    Jack
+                </div>
+                <div className={classes.dialog}>
+                    Joel
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hello World!</div>
+                <div className={classes.message}>React is Awesome!</div>
+                <div className={classes.message}>Think different</div>
+            </div>
         </div>
     )
 }
