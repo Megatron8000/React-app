e62820691739658c08c3a0f39f83ac98546e06f1
Alexander <barykin88@hotmail.com>
2020-06-14 14:54:13 +0400

    modified:   src/App.js
    Used 'render' instead of 'component' in Route

--- 01-firstapp/hello/src/App.js
+++ 01-firstapp/hello/src/App.js
@@ -18,11 +18,11 @@ const App = () => {
         <Header />
         <Navbar />
         <div className='app-wrapper-content'>
-          <Route path='/profile' component={Profile} />
-          <Route path='/dialogs' component={Dialogs} />
-          <Route path='/news' component={News} />
-          <Route path='/music' component={Music} />
-          <Route path='/settings' component={Settings} />
+          <Route path='/profile' render={ () => <Profile /> } />
+          <Route path='/dialogs' render={ () => <Dialogs /> } />
+          <Route path='/news' render= { () => <News /> } />
+          <Route path='/music' render= { () => <Music /> } />
+          <Route path='/settings' render= { () => <Settings /> } />
         </div>
       </div>
     </BrowserRouter>
