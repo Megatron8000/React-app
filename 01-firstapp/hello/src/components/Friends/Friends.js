6f87b8b3a621c9c1b192c739c33e25987533fdc5
Alexander <barykin88@hotmail.com>
2020-06-22 22:07:09 +0400

    new file:   src/components/Friends/Brian.jpg 	modified:   src/components/Friends/Friends.js 	modified:   src/components/Friends/Friends.module.css 	new file:   src/components/Friends/Peter.jpeg 	new file:   src/components/Friends/Quagmire.jpg 	modified:   src/components/Navbar/Navbar.module.css
    Продолжаю хардкодить френдлист, нужно придумать как передавать изображения в массиве

--- 01-firstapp/hello/src/components/Friends/Friends.js
+++ 01-firstapp/hello/src/components/Friends/Friends.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import classes from './Friends.module.css';
-
+import Quagmire from './Quagmire.jpg'
+import Brian from './Brian.jpg'
+import Peter from './Peter.jpeg'
 
 const Friends = () => {
     return  <div className={classes.container}>
     <div className={classes.subtitle}>My friends</div>
+    
     <div className={classes.friend}>
-      friend
+      <img src={Brian} alt='Brian' />
     </div>
     <div className={classes.friend}>
-      friend
+      <img src={Quagmire} alt='Quagmire' />
     </div>
     <div className={classes.friend}>
-      friend
-    </div>
-    <div className={classes.friend}>
-      friend
+      <img src={Peter} alt='Peter' />
     </div>
   </div>
   }
