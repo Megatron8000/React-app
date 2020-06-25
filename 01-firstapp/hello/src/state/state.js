851754c2e32fdbcc5ff604bb25eab330159c08b3
Alexander <barykin88@hotmail.com>
2020-06-25 22:09:28 +0400

    modified:   src/state/state.js
    сделал массив с данными под аватары друзей

--- 01-firstapp/hello/src/state/state.js
+++ 01-firstapp/hello/src/state/state.js
@@ -1,3 +1,20 @@
+import React from 'react';
+import Quagmire from './Quagmire.jpg'
+import Brian from './Brian.jpg'
+import Peter from './Peter.jpeg'
+
+
+
+
+//черновик под фотки
+
+const brian  = <img src={Brian} alt='Brian' />
+const peter  = <img src={Peter} alt='Peter' />
+const quagmire  = <img src={Quagmire} alt='Brian' />
+
+
+
+
 let state = {
 
     dialogsPage: {    //объект хранящий данные для страницы сообщений
@@ -31,10 +48,14 @@ let state = {
             { id: 2, post: 'React is awesome!', likes: 26 }
         ]
 
-    }
+    },
 
    
-
+    avatars: [
+        {id: 1, value: brian},
+        {id: 2, value: peter},
+        {id: 3, value: quagmire}
+    ]
  
 }
 
