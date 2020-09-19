a02595b0652ce8bda572208665b2663e8d3465ea
Alexander <barykin88@hotmail.com>
2020-09-19 12:01:14 +0400

    modified:   src/index.js Рефакторинг индекса

--- 01-firstapp/hello/src/index.js
+++ 01-firstapp/hello/src/index.js
@@ -8,7 +8,7 @@ import App from './App';
 let rerenderEntireTree = (state) => {
   ReactDOM.render(
     <React.StrictMode>
-      <App state = {state} dispatch={store.dispatch.bind(store)}  store={store} />
+      <App state = {state} dispatch={store.dispatch.bind(store)}/>
     </React.StrictMode>,
     document.getElementById('root')
   );
