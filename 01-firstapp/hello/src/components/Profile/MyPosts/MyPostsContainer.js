ba1d9556f3e86ea66dd0a67d0dec027c58fd855c
Alexander <barykin88@hotmail.com>
2020-09-20 12:39:27 +0400

    modified:   src/components/Dialogs/DialogsContainer.js 	modified:   src/components/Profile/MyPosts/MyPostsContainer.js

--- 01-firstapp/hello/src/components/Profile/MyPosts/MyPostsContainer.js
+++ 01-firstapp/hello/src/components/Profile/MyPosts/MyPostsContainer.js
@@ -2,24 +2,20 @@ import React from 'react';
 import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../state/profile-reducer';
 import MyPosts from './MyPosts';
 
-const MyPostsContainer = (props) => { 
+const MyPostsContainer = (props) => {
 
   let addPost = () => {
     props.dispatch(addPostActionCreator())
   }
-  // создает функцию, которая создает новый объект пост
-
   let onTextChange = (text) => {
     props.dispatch(updateNewPostTextActionCreator(text))
   }
-  // позволяет изменить строго заданное содержимое текстэреа
-
-  return(<MyPosts 
-      updateNewPostText={onTextChange} 
-      addPost={addPost}
-      postData={props.postData}
-      newPostText={props.newPostText}
-       />)
+  return (<MyPosts
+    updateNewPostText={onTextChange}
+    addPost={addPost}
+    postData={props.postData}
+    newPostText={props.newPostText}
+  />)
 }
 
 export default MyPostsContainer
